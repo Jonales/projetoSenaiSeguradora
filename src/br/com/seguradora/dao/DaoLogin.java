90715b79af2fb6505c56c7395ead19f9af52309f
jonales <jonales@gmail.com>
2023-01-31 21:11:24 -0300

    0.0.2

--- src/br/com/seguradora/dao/DaoLogin.java
+++ src/br/com/seguradora/dao/DaoLogin.java
@@ -8,46 +8,69 @@ import br.com.seguradora.connection.ConnectionFactory;
 import br.com.seguradora.modelo.ModeloLogin;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import javax.swing.JOptionPane;
 
 /**
  *
  * @author Jon
  */
 public class DaoLogin {
+
     private Connection connection;
-    
-    public DaoLogin(){
-       this.connection = new ConnectionFactory().getConnection();
+
+    public DaoLogin() {
+        this.connection = new ConnectionFactory().getConnection();
     }
-    
+
+    public ResultSet autenticacaoLogin(ModeloLogin modeloLogin) {
+        connection = (Connection) new DaoLogin();
+        
+        try {
+            String sql = "select * from login where login = ? and senha = ? ";
+            PreparedStatement stmt = connection.prepareStatement(sql);
+            stmt.setString(1, modeloLogin.getLogin());
+            stmt.setString(2, modeloLogin.getSenha());
+            
+            ResultSet rs = stmt.executeQuery();
+            return rs;
+
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, "DAOLogin" + e);
+            return null;
+        }
+    }
+
     /**
      * Insere um usuario dentro do banco de dados
+     *
      * @param usuario exige que seja passado um objeto do tipo usuario
      */
-    public void insert(ModeloLogin usuario){
+    public void insert(ModeloLogin usuario) {
         String sql = "insert into seguradora.login (cargo, nome, login, senha) VALUES(?,?,?,?)";
         try {
-           PreparedStatement stmt = connection.prepareStatement(sql);
-           stmt.setString(1, usuario.getCargo());
-           stmt.setString(2, usuario.getNome());
-           stmt.setString(3, usuario.getLogin());
-           stmt.setString(4, usuario.getSenha());
-           stmt.execute();
-           stmt.close();
+            PreparedStatement stmt = connection.prepareStatement(sql);
+            stmt.setString(1, usuario.getCargo());
+            stmt.setString(2, usuario.getNome());
+            stmt.setString(3, usuario.getLogin());
+            stmt.setString(4, usuario.getSenha());
+            stmt.execute();
+            stmt.close();
 
         } catch (SQLException u) {
             throw new RuntimeException(u);
         }
     }
-    
+
     /**
      * Atualiza um Objeto no banco de dados
+     *
      * @param usuario
-     * @return 
+     * @return
      */
-    public boolean update(ModeloLogin usuario){
+    public boolean update(ModeloLogin usuario) {
         /*
         for (int i = 0; i < banco.usuario.size(); i++) {
             if(idSaoIguais(Banco.usuario.get(i),usuario)){
@@ -55,15 +78,16 @@ public class DaoLogin {
                 return true;
             }
         }*/
-        return false;      
+        return false;
     }
-    
+
     /**
      * Deleta um objeto do banco de dados pelo id do usuario passado
+     *
      * @param usuario
-     * @return 
+     * @return
      */
-    public boolean delete(ModeloLogin usuario){
+    public boolean delete(ModeloLogin usuario) {
         /*
         for (ModeloLogin usuarioLista : Banco.usuario) {
             if(idSaoIguais(usuarioLista,usuario)){
@@ -73,21 +97,24 @@ public class DaoLogin {
         }*/
         return false;
     }
-    
+
     /**
      * Retorna um arraylist com todos os usuarios do banco de dados
+     *
      * @return uma lista com todos os registros do banco
      */
     /*public ArrayList<ModeloLogin> selectAll(){
         return Banco.usuario;
     }
-    */
+     */
     /**
-     * Retorna um Objeto do tipo usuario se a funcao encontrar o usuario passado como parâmetro no banco, para considerar sao usado nome e senha
+     * Retorna um Objeto do tipo usuario se a funcao encontrar o usuario passado
+     * como parâmetro no banco, para considerar sao usado nome e senha
+     *
      * @param usuario
      * @return Usuario encontrado no banco de dados
      */
-    public ModeloLogin selectPorNomeESenha(ModeloLogin usuario){
+    public ModeloLogin selectPorNomeESenha(ModeloLogin usuario) {
         /*
         for (ModeloLogin usuarioLista : Banco.usuario) {
             if(nomeESenhaSaoIguais(usuarioLista,usuario)){
@@ -99,6 +126,7 @@ public class DaoLogin {
 
     /**
      * Recebe dois objetos e verifica se são iguais verificando o nome e senha
+     *
      * @param usuario
      * @param usuarioAPesquisar
      * @return verdadeiro caso sejam iguais e falso caso nao forem iguais
@@ -109,12 +137,13 @@ public class DaoLogin {
 
     /**
      * Compara se dois objetos tem a propriedade id igual
+     *
      * @param usuario
      * @param usuarioAComparar
      * @return verdadeiro caso os id forem iguais e falso se nao forem
      */
     private boolean idSaoIguais(ModeloLogin usuario, ModeloLogin usuarioAComparar) {
-        return usuario.getId() ==  usuarioAComparar.getId();
+        return usuario.getId() == usuarioAComparar.getId();
     }
-    
+
 }
